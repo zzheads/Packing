1167cdca4841adf516e2b351b93d064bc11c5963
Alexey Papin <zzheads@gmail.com>
2016-12-29 10:43:39 +0300

    Initial Commit

---
+++ Packing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Packing
+//
+//  Created by Alexey Papin on 29.12.16.
+//  Copyright © 2016 zzheads. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
